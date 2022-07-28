8fcf4996fa20430a5c4ee1ce29b715409c232d37
Natalie Salazar <natmsal33@gmail.com>
2022-07-27 21:49:31 -0700

    first commit, fully functional HTML/JS

---
+++ script.js
@@ -0,0 +1,37 @@
+var taskForm = document.getElementById('taskInput');
+var taskList = document.getElementById('tasks');
+
+// Add new task
+taskForm.addEventListener('submit', addTask);
+// Delete task
+taskList.addEventListener('click', removeTask);
+
+function addTask(e){
+    e.preventDefault();
+    var newTask = document.getElementById('newTask').value;
+    
+    // Create new li element
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(newTask));
+
+    // Create delete button
+    var dBtn = document.createElement('button');
+    dBtn.appendChild(document.createTextNode('X'));
+    dBtn.className = 'delete';
+    li.appendChild(dBtn);
+
+    // Add item to task list
+    taskList.appendChild(li);
+
+}
+
+function removeTask(click) {
+    if(click.target.classList.contains('delete')) {
+        var li = click.target.parentElement;
+        taskList.removeChild(li);
+    } else if(click.target.className === 'strikethrough'){
+        click.target.classList.remove('strikethrough');
+    } else {
+        click.target.className = 'strikethrough';
+    }
+}
